0d98c95f22744c8efa14b5327ec493c617d055ac
Nicolas Morel <nicolas.morel@fr.clara.net>
2024-02-01 11:42:40 +0100

    Add python script for SFX count snapshot metrics

--- purge_snapshot.py
+++ purge_snapshot.py
@@ -1,11 +1,7 @@
 import logging
-import signalfx
 import os
-import time
-import atexit
-from operator import itemgetter, attrgetter
+from operator import attrgetter
 from lib import azurenfssnapshot
-from datetime import datetime
 
 logger = logging.getLogger("nfs_snapshot_purge")
 log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
@@ -14,31 +10,10 @@ sh = logging.StreamHandler()
 sh.setLevel(log_level)
 logger.addHandler(sh)
 
-sfx_logger = logging.getLogger("signalfx.ingest")
-sfx_logger.setLevel(log_level)
-sfx_logger.addHandler(sh)
 
 def run():
     logger.info("Starting job")
-    sfx_token = os.environ.get("SFX_TOKEN")
-    snapshot_history = os.environ.get("SNAPSHOT_HISTORY", 2)
-    if not sfx_token:
-        raise ValueError("Environment variable SFX_TOKEN not set")
-
-    sfx_realm = os.environ.get("SFX_REALM", "eu0")
-    logging.info(f"SignalFx realm: {sfx_realm}")
-
-    extra_dimensions = {
-        couple.split("=")[0]: couple.split("=")[1]
-        for couple in os.environ.get("SFX_EXTRA_DIMENSIONS", "").split(",")
-        if couple != ""
-    }
-    logging.info(f"Extra signalFx dimensions: {extra_dimensions}")
-    sfx_clt = signalfx.SignalFx(
-        api_endpoint=f"https://api.{sfx_realm}.signalfx.com",
-        ingest_endpoint=f"https://ingest.{sfx_realm}.signalfx.com",
-        stream_endpoint=f"https://stream.{sfx_realm}.signalfx.com",
-    )
+    snapshot_history = os.environ.get("SNAPSHOT_HISTORY", 7)
 
     logging.info("Init AzureNfsSnapshot class")
     az = azurenfssnapshot.AzureNfsSnapshot()
@@ -49,19 +24,12 @@ def run():
         snapshots_list = sorted(az.list_snapshot(storage_account=sa),
                                 key=attrgetter('snapshot'))
         snapshot_diff = len(snapshots_list) - snapshot_history
-        print(
+        logger.info(
             f"{sa} - Nb Snapshot : {len(snapshots_list)} - Diff - {str(snapshot_diff)}")
         if len(snapshots_list) > snapshot_history:
             for snapshot in snapshots_list[:snapshot_diff]:
-                print(f"Delete {snapshot.snapshot} on {sa} - {snapshot.name}")
+                logger.info(f"Delete {snapshot.snapshot} on {sa} - {snapshot.name}")
                 # az.delete_snapshot(storage_account=sa, share=snapshot.name, snapshot=snapshot.snapshot)
-
-    logging.info("Begin collect metrics for splunk")
-    with sfx_clt.ingest(sfx_token) as sfx:
-        atexit.register(sfx.stop)
-        for
-
-
 
     logging.info("Job Done")
 
---
+++ snapshots_metric.py
@@ -0,0 +1,75 @@
+import logging
+import signalfx
+import os
+import time
+import atexit
+from lib import azurenfssnapshot
+from datetime import datetime
+
+logger = logging.getLogger("nb_nfs_snapshot")
+log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
+logger.setLevel(log_level)
+sh = logging.StreamHandler()
+sh.setLevel(log_level)
+logger.addHandler(sh)
+
+sfx_logger = logging.getLogger("signalfx.ingest")
+sfx_logger.setLevel(log_level)
+sfx_logger.addHandler(sh)
+
+def run():
+    logger.info("Starting job")
+    sfx_token = os.environ.get("SFX_TOKEN")
+    if not sfx_token:
+        raise ValueError("Environment variable SFX_TOKEN not set")
+    sfx_realm = os.environ.get("SFX_REALM", "eu0")
+    logging.info(f"SignalFx realm: {sfx_realm}")
+
+    extra_dimensions = {
+        couple.split("=")[0]: couple.split("=")[1]
+        for couple in os.environ.get("SFX_EXTRA_DIMENSIONS", "").split(",")
+        if couple != ""
+    }
+    logging.info(f"Extra signalFx dimensions: {extra_dimensions}")
+    sfx_clt = signalfx.SignalFx(
+        api_endpoint=f"https://api.{sfx_realm}.signalfx.com",
+        ingest_endpoint=f"https://ingest.{sfx_realm}.signalfx.com",
+        stream_endpoint=f"https://stream.{sfx_realm}.signalfx.com",
+    )
+
+    logging.info("Init AzureNfsSnapshot class")
+    az = azurenfssnapshot.AzureNfsSnapshot()
+
+    with sfx_clt.ingest(sfx_token) as sfx:
+        atexit.register(sfx.stop)
+        logging.info("Collect metric for number of snapshots")
+
+        for sa, value in az.filestorage_list.items():
+            for share in az.list_shares(storage_account=sa):
+                sfx_gauge_value = len(az.list_snapshot(storage_account=sa, share_name=share.name))
+
+                metric_dimensions = {
+                    **dict({'storage_account_name': sa,
+                            'subscription_id': value.subscription_id,
+                            'env': value.tags['env'],
+                            'share_name': share.name
+                            }),
+                    **extra_dimensions
+                }
+
+                sfx_metric = [
+                    {
+                        'metric': 'fame.azure.nfs.nb_snapshot',
+                        'value': sfx_gauge_value,
+                        'timestamp': round(time.time() * 1000),
+                        'dimensions': metric_dimensions
+                    }
+                ]
+                logging.debug(
+                    f"send this metric for {sa} - {share.name} : {sfx_metric}")
+                sfx.send(gauges=sfx_metric)
+
+    logging.info("Job Done")
+
+if __name__ == "__main__":
+    run()
