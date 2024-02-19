2b7e17b5839e6c966cba4b9abb639c5968475390
Nicolas Morel <nicolas.morel@fr.clara.net>
2024-02-19 11:10:15 +0100

    Add outgoing ip on log

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ from azure_nfs_snapshot import create_snapshot, purge_snapshot, snapshots_metric
 import azure.functions as func
 import logging
 import os
+import requests
 
 log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
 logging.basicConfig(level=logging.getLevelName(log_level), format='%(asctime)s %(levelname)-8s %(message)s')
@@ -18,6 +19,7 @@ app = func.FunctionApp()
               use_monitor=False) 
 def NfsSnapshot(myTimer: func.TimerRequest) -> None:
 
+    logging.info(f"Outgoing Ip: {requests.get('https://ip.clara.net').text}")
     if myTimer.past_due:
         logging.info('The timer is past due!')
     logging.info('NfsSnapshot - Running - create_snapshot.run()')
